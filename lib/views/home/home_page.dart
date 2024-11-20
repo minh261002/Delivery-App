3bc54955f373ac3f83e9efd7328911685589fadf
minhttran <trcongminh.261002@gmail.com>
2024-11-20 16:49:51 +0700

    Custom container

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -1,14 +1,22 @@
+import 'package:delivery_app/common/custom_container.dart';
+import 'package:delivery_app/constants/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Page'),
+    return Scaffold(
+      backgroundColor: kPrimary,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(130.h),
+        child: Container(
+          height: 130,
+        ),
       ),
+      body: SafeArea(child: CustomContainer(containerContent: Container())),
     );
   }
 }
